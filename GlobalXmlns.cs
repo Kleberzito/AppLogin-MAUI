3200f861d9118ebeadc4eb9ba21e27c49cb57720
Kleber G. da Silva <kleber.gsilva@outlook.com>
2026-02-16 20:02:16 -0300

    chore: commit inicial - Aplicativo de login MAUI com .gitignore

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LoginApp")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LoginApp.Pages")]
---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LoginApp.Views;
+
+namespace LoginApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnEntrarClicked(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string senha = txtSenha.Text;
+
+            if (usuario == "admin" && senha == "1234")
+            {
+                await Navigation.PushAsync(new WelcomePage());
+            }
+            else 
+            {
+                lblMensagem.Text = "Usuario ou senha inválidos!";
+            }
+        }
+    }
+}
